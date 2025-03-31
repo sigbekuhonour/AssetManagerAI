bf788a3136277af96f3d11e1890f74650cc419b6
Honour Sigbeku <sigbekuhonour@gmail.com>
2025-03-31 19:19:25 -0230

    commented the entire process file function

--- src/app/uploadFile.tsx
+++ src/app/uploadFile.tsx
@@ -1,39 +1,38 @@
-import { supabase } from "@/lib/supabase";
-import OpenAI from "openai";
-const openai = new OpenAI();
+// import OpenAI from "openai";
+// const openai = new OpenAI();
 
-async function processFile(fileName: string) {
-    try {
-      // Download file from Supabase Storage
-      // const { data, error } = await supabase
-      //   .storage
-      //   .from('assets')
-      //   //in this case the id might actually end up being the file name
-      //   .download(fileName);
+// async function processFile(fileName: string) {
+//     try {
+//       // Download file from Supabase Storage
+//       // const { data, error } = await supabase
+//       //   .storage
+//       //   .from('assets')
+//       //   //in this case the id might actually end up being the file name
+//       //   .download(fileName);
   
-      if (error) {
-        throw new Error(`Error downloading file: ${error.message}`);
-      }
+//       // if (error) {
+//       //   throw new Error(`Error downloading file: ${error.message}`);
+//       // }
   
-      // Convert data to a readable stream
-      // Convert data to a File object
-      const blob = new Blob([data]);
-      const file = new File([blob], fileName);
+//       // Convert data to a readable stream
+//       // Convert data to a File object
+//       const blob = new Blob([data]);
+//       const file = new File([blob], fileName);
   
-      // Upload to OpenAI
-      const uploadedFile = await openai.files.create({
-        file: file,
-        purpose: "assistants"
-      });
-      console.log(`file with ID: ${uploadedFile.id}`);
-      return file;
+//       // Upload to OpenAI
+//       const uploadedFile = await openai.files.create({
+//         file: file,
+//         purpose: "assistants"
+//       });
+//       console.log(`file with ID: ${uploadedFile.id}`);
+//       return file;
     
-    } catch (error) {
-    if (error instanceof Error) {
-      console.error("Error:", error.message);
-    } else {
-      console.error("Unexpected error:", error);
-    }
-    }
-  }
+//     } catch (error) {
+//     if (error instanceof Error) {
+//       console.error("Error:", error.message);
+//     } else {
+//       console.error("Unexpected error:", error);
+//     }
+//     }
+//   }
   
