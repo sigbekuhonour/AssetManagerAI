78521c2b0cefb8c483bf5fe9b7dc10ae08fcd21a
Honour0512 <63160822+sigbekuhonour@users.noreply.github.com>
2025-05-21 23:09:18 -0230

    removed the anon key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
 const anonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-console.log('Anon key', anonKey)
+// console.log('Anon key', anonKey)
 const supabase = createClient('https://ijqoznneorbxabhkcgzl.supabase.co', anonKey)
 
 export { supabase }
